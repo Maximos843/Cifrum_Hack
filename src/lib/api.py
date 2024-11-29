2f47c8a57f2ac8dfdbd2fe730b008b7ecc6d1d32
Rushan <50203332+Tishran@users.noreply.github.com>
2024-11-29 11:17:32 +0300

    simple api endpoints (#1)
    * simple api endpoints
    
    * change project structure
    
    move run.py into /src/lib folder
    rename run.py into app.py and added api.py where routers are located
    "splited" /predict router into HTMLResponse and JSONResponse type

---
+++ src/lib/api.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, HTTPException, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+
+
+router = APIRouter()
+templates = Jinja2Templates(directory="src/lib/template")
+
+
+@router.post("/predict")
+async def predict(request: Request, review_text: str = Form(...)):
+    try:
+        # TODO: get predictions from model
+        predictions = {"prediction": "<placeholder>"}
+        
+        if "text/html" in request.headers.get("Accept", ""):
+            return templates.TemplateResponse("index.html", {"request": request, "predictions": predictions, "title": "Predictions"})
+        else:
+            return JSONResponse(content=predictions)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ src/lib/app.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import uvicorn
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from api import router
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="src/lib/static"), name="static")
+templates = Jinja2Templates(directory="src/lib/template")
+
+app.include_router(router)
+
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "Home"})
+
+
+def start_service():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    start_service()
